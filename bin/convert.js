7553a5d6b063404be706d9f8cd24d7f2144f0e23
zz85 <zz85nus@gmail.com>
2013-04-07 14:37:00 +0800

    Improve convert.js

--- bin/convert.js
+++ bin/convert.js
@@ -1,19 +1,20 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
-var spawn = require('child_process').spawn;
 
 // based on http://stackoverflow.com/a/13928240
 
-var format = '{%n"hash":"%h","parents":"%p",%n"author":"%an",%n"date":"%at",%n"message":"%s",%n"commitDate":"%ct"}'.replace(/\"/g, '^@^');
+var format = '{"hash":"%h","parents":"%p","author":"%an","date":"%at","message":"%s","commitDate":"%ct"}'.replace(/\"/g, '^@^');
 var cmd = 'git log --pretty=format:\'' + format + ',\' > result.json';
-var cwd = ''; // target git repository directory
+var cwd = '../three.js/'; // target git repository directory
 var target = 'data/test.json';
 
-var fileschanged = 'git log --name-only --pretty="__HASH__%h"'
+var fileschanged = 'git log --name-status --pretty="__HASH__%h"'
 // --name-status --name-only
 
+var pretty_json = !true;
 var hash = {};
 
+// AMD - Add, Modified, Delete
 getfiles();
 
 function getfiles() {
@@ -28,17 +29,24 @@ function getfiles() {
 		var files = fs.readFileSync(cwd + '__files.txt', 'utf8');
 		fs.unlinkSync(cwd + '__files.txt');
 
-
 		files = files.split('__HASH__')
 		files.shift()
 
+		var f, names;
+
+
 		for (var i=0;i<files.length;i++) {
-			var names = files[i].split('\n\n');
-			var f = names[1].split('\n')
-			f.pop();
+			names = files[i].split('\n\n');
+			if (names.length>1) {
+				f = names[1].split('\n')
+				f.pop();
+			} else {
+				f = [];
+			}
 			hash[names[0]] = f;
 		}
 
+		// console.log(hash);
 
 		var child = exec(cmd, {cwd: cwd},
 			function (error, stdout, stderr) {
@@ -51,27 +59,6 @@ function getfiles() {
 
 	});
 
-	// var changedfiles = [];
-	// var args = fileschanged.split(' ');
-	// closure = spawn(args[0], args.slice(1))
-
-	// closure.stdout.on('data', function (data) {
-	// 	changedfiles.push('' + data);
-	// });
-
-	// closure.stderr.on('data', function (data) {
-	// 	console.error('' + data)
-	// });
-
-	// closure.on('exit', function (code) {
-	// 	if (code) {
-	// 		return;
-	// 	}
-
-	// 	changedfiles = changedfiles.join();
-	// 	console.log(changedfiles);
-	// });
-
 }
 
 
@@ -79,23 +66,27 @@ function convert() {
 
 	var result = fs.readFileSync(cwd + 'result.json', 'utf8');
 	fs.unlinkSync(cwd + 'result.json');
-	var out = result.replace(/"/gm, '\\"').replace(/\^@\^/gm, '"').replace(/w+/g, ' ');
+	var out = result.replace(/"/gm, '\\"').replace(/\^@\^/gm, '"');
 	if (out[out.length - 1] == ',') {
 		out = out.substring (0, out.length - 1);
 	}
 
 	// quick hack!
 	var log = eval('[' + out + ']');
+	// var log = JSON.parse('[' + out + ']');
 	var commit;
 
 	console.log(hash);
 
 	for (var i=0;i<log.length;i++) {
 		commit = log[i];
-		commit.files = hash[commit.hash]
+		commit.files = hash[commit.hash];
+		commit.parents = commit.parents.split(' ');
+		commit.date = parseInt(commit.date);
 	}
 
-	var json = JSON.stringify(log, null, '\t');
+	var json = JSON.stringify(log, null,
+		pretty_json ? '\t' : '');
 
 	fs.writeFileSync(target, json, 'utf8');
 
