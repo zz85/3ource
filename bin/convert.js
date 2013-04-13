e17ae6a165eb34b6833536a6747306fd599fa349
zz85 <zz85nus@gmail.com>
2013-04-13 11:36:44 +0800

    Creating a more compact json format for git logs
    2.1M json prettify
    1.8M json non-prettify
    
    1.7M json packed prettify
    1.5M json packed non-prettify

--- bin/convert.js
+++ bin/convert.js
@@ -1,17 +1,30 @@
 var exec = require('child_process').exec;
 var fs = require('fs');
 
-// based on http://stackoverflow.com/a/13928240
-
-var format = '{"hash":"%h","parents":"%p","author":"%an","date":"%at","message":"%s","commitDate":"%ct"}'.replace(/\"/g, '^@^');
-var cmd = 'git log --pretty=format:\'' + format + ',\' > result.json';
+// Options
 var cwd = '../three.js/'; // target git repository directory
 var target = 'data/test.json';
+var pretty_json = true;
+var pack_json = true;
+// End Options
+
+var json_format = {
+	"hash":"%h",
+	"parents":"%p",
+	"author":"%an",
+	"date":"%at",
+	"message":"%s",
+	"commitDate":"%ct",
+	"files": ""
+};
+
+var pretty_format = JSON.stringify(json_format).replace(/\"/g, '^@^');
+var cmd = 'git log --pretty=format:\'' + pretty_format + ',\' > result.json';
 
 var fileschanged = 'git log --name-status --pretty="__HASH__%h"'
 // --name-status --name-only
 
-var pretty_json = true;
+
 var hash = {};
 
 // AMD - Add, Modified, Delete
@@ -38,7 +51,8 @@ function getfiles() {
 		for (var i=0;i<files.length;i++) {
 			names = files[i].split('\n\n');
 			if (names.length>1) {
-				f = names[1].split('\n')
+				f = names[1]
+					.split('\n')
 				f.pop();
 			} else {
 				f = [];
@@ -61,6 +75,42 @@ function getfiles() {
 
 }
 
+function json_pack(a) {
+	// From [{a, b, c}, {a, b, c}] => {a:[], b:[], c:[]}
+	var packed = {}, k;
+	for (k in json_format) {
+		packed[k] = [];
+	}
+	var i,il, e;
+	for (i=0, il=a.length; i<il; i++) {
+		e = a[i];
+		for (k in json_format) {
+			packed[k].push(e[k]);
+		}
+	}
+	return packed;
+}
+
+function json_unpack(packed) {
+	// From {a:[], b:[], c:[]} => [{a, b, c}, {a, b, c}]
+	var unpacked = [], k, il;
+	for (k in packed) {
+		il = packed[k].length;
+		break;
+	}
+
+	var i, o;
+	for (i=0;i<il;i++) {
+		o = {};
+		for (k in packed) {
+			o[k] = packed[k][i];
+		}
+		unpacked.push(o);
+	}
+	return unpacked;
+
+}
+
 
 function convert() {
 
@@ -76,7 +126,7 @@ function convert() {
 	// var log = JSON.parse('[' + out + ']');
 	var commit;
 
-	console.log(hash);
+	// console.log(hash);
 
 	for (var i=0;i<log.length;i++) {
 		commit = log[i];
@@ -84,6 +134,8 @@ function convert() {
 		commit.parents = commit.parents != '' ? commit.parents.split(' '): [];
 		commit.date = parseInt(commit.date);
 	}
+
+	if (pack_json) log = json_pack(log);
 
 	var json = JSON.stringify(log, null,
 		pretty_json ? '\t' : '');
